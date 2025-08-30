8df6e7c85fe667ba25226e6db0752b7ab1b06d7b
olegador4ik <olegador4ik2008@gmail.com>
2025-08-30 13:13:51 +0300

    'r'

--- pct/settings.py
+++ pct/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#qa8_@ec@kvgeb5x409sfbz)7ol^0o4h59a9c_t)#+1%5*#mdm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "pcmch.onrender.com"]
 
