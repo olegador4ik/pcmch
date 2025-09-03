0ad20a572d9c486408337889ccc3c14e5cf9be65
olegador4ik <olegador4ik2008@gmail.com>
2025-09-03 19:59:40 +0300

    5

--- pctm/views.py
+++ pctm/views.py
@@ -26,31 +26,49 @@ def custom_404(request, exception):
 
 def main(request):
     template = loader.get_template("main.html")
+    ua = request.META.get("HTTP_USER_AGENT", "").lower()
+    if "mobile" in ua:
+        is_mobile = True
+    else:
+        is_mobile = False
     context = {
-        'lgs': lgs
+        'lgs': lgs,
+        'im': is_mobile
     }
     return HttpResponse(template.render(context, request))
 
 def question(request):
     template = loader.get_template("question.html")  
+    ua = request.META.get("HTTP_USER_AGENT", "").lower()
     qs = Q.objects.all().values('id', 'n', 'rus', 'eng')
     qs_json = json.dumps(list(qs), cls=DjangoJSONEncoder)
+    if "mobile" in ua:
+        is_mobile = True
+    else:
+        is_mobile = False
     context = {
         'qs_json': qs_json,
-        'lgs': lgs
+        'lgs': lgs,
+        'im': is_mobile
     }
     return HttpResponse(template.render(context, request))
 
 def result(request):
     template = loader.get_template("result.html")
+    ua = request.META.get("HTTP_USER_AGENT", "").lower()
     ids = I.objects.all().values('id', 'c', 'x', 'y', 'w', 'h')
     ivs = IT.objects.all().values('id', 'q', 'rus', 'eng')
     is_json = json.dumps(list(ids), cls=DjangoJSONEncoder)
     iv_json = json.dumps(list(ivs), cls=DjangoJSONEncoder)
+    if "mobile" in ua:
+        is_mobile = True
+    else:
+        is_mobile = False
     context = {
         'is_json': is_json,
         'iv_json': iv_json,
-        'lgs': lgs
+        'lgs': lgs,
+        'im': is_mobile
     }  
     return HttpResponse(template.render(context, request))
 
